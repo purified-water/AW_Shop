1c1f7dfaa9a0e24a4d098194c3e83b5674e3926e
21120588 <julianng1110@gmail.com>
2024-01-12 14:55:21 +0700

    Thêm hiển thị list categories và products; thêm điều hướng nhấnHome ở navbar

--- models/cate.m.js
+++ models/cate.m.js
@@ -3,11 +3,7 @@ const db = require('../utils/db');
 module.exports = {
     getCates: () => {
         try {
-            cateList = [
-                { id: 1, name: 'Category 1' },
-                { id: 2, name: 'Category 2' },
-                { id: 3, name: 'Category 3' },
-            ];
+            cateList = db.getCategories();
             // const cateList = await db.getAll('Category');
             return cateList;
         }
