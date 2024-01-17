116fda534f7d8274bea625d8a612ff1630146b18
21120575 <nguyen.thanhtri2112@gmail.com>
2024-01-17 22:33:21 +0700

    cai dat detail product ui

--- controllers/prod.c.js
+++ controllers/prod.c.js
@@ -42,5 +42,12 @@ module.exports = {
         } catch (error) {
             next(error);
         }
+    },
+    loadDetail: (req, res, next) => {
+        try {
+            res.render("detail");
+        } catch (error){
+            next(error);
+        }
     }
 }
