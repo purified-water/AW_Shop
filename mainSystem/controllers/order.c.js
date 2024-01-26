9c3cbcd3361ba3997bba37634b679cacefe305de
21120588 <julianng1110@gmail.com>
2024-01-26 17:50:06 +0700

    Thêm giao diện cho hiển thị Orders cho Admin

--- mainSystem/controllers/order.c.js
+++ mainSystem/controllers/order.c.js
@@ -1,15 +1,17 @@
 const shop_order = require('../models/shop_order.m')
 const detail_order = require('../models/detail_order.m')
 const db = require('../utils/db');
+
 module.exports = {
     loadOrder: async (req,res,next) => {
         try {       
-            const order = await db.getAll('shop_order')
+            let nav = await db.getCategories()
+            const orders = await db.getAll('shop_order')
             // console.log(orderDay)
-            res.render('analyze',{
+            res.render('order',{
                 user: req.user[0],
-                order: order,
-                pageTitle: "Analyze",
+                orders: orders,
+                pageTitle: "Orders",
                 cateListNav: nav,
             });
         } catch (error) {
@@ -18,9 +20,11 @@ module.exports = {
     },
     loadDetailOrder: async (req, res, next) => {
         try {       
+            let nav = await db.getCategories()
+
             const detail = await detail_order.getDetailOrder(req.body.order.order_id);
             // console.log(orderDay)
-            res.render('analyze',{
+            res.render('orderDetail',{
                 user: req.user[0],
                 detail: detail,
                 pageTitle: "Analyze",
