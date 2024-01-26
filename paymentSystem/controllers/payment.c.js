5434a8378cef867c026b7c31d1142dfa8bd5d26b
21120588 <julianng1110@gmail.com>
2024-01-26 17:25:53 +0700

    Thêm tiền cho admin sau thanh toán, sửa edit product không hiển thị modal, thêm tiểu tiết UI

--- paymentSystem/controllers/payment.c.js
+++ paymentSystem/controllers/payment.c.js
@@ -1,11 +1,12 @@
 const paymentModel = require('../models/payment.m')
+const db = require('../utils/db');
 module.exports = {
     payWithWallet: async (req, res, next) => {
         try {
             const paymentInfo = req.body.shopOrder;
             // console.log('Payment info: ', paymentInfo);
             const userID = req.body.user_id;
-            console.log('User info', userID);
+            // console.log('User info', userID);
             const account = await paymentModel.getAccount(userID);
             if (account.length == 0) {
                 return res.status(400).json({
@@ -19,9 +20,19 @@ module.exports = {
                 })
             }
 
+            //Lấy user info của admin
+            const adminUser = await db.getCondition('users', 'username', 'admin');
+            const adminUserID = adminUser[0].id;
+
+            //Lấy account info của admin
+            const adminAccount = await paymentModel.getAccount(adminUserID);
+            const adminID = adminAccount[0].id;
+            //Trừ tiền của client và cộng tiền cho admin
             const newBalance = account[0].balance - paymentInfo.total;
+            const newAdminBalance = adminAccount[0].balance + paymentInfo.total;
             console.log('New balance: ', newBalance);
             await paymentModel.updateAccount(userID, newBalance);
+            await paymentModel.updateAccount(adminUserID, newAdminBalance);
             res.status(200).json({ message: 'Payment success' });
         } catch (error) {
             res.status(400).json({
